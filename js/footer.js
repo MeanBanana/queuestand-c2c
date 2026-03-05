9ed3d03642137cb6141e9f12f8c23177bb0d1bfe
Carel von Ronge <carel.vonronge@gmail.com>
2026-03-06 01:23:31 +0200

    Skeloton + CSS

---
+++ js/footer.js
@@ -0,0 +1,5 @@
+document.write(`
+    <footer>
+        <p>&copy; 2026 QueueStand. All rights reserved.</p>
+    </footer>
+`);
---
+++ js/navbar.js
@@ -0,0 +1,17 @@
+document.write(`
+<header>
+  <div>
+    <h1><a href="index.html">QueueStand</a></h1>
+    <nav>
+      <ul>
+        <li><a href="about.html">About Us</a></li>
+        <li><a href="how-it-works.html">How It Works</a></li>
+        <li><a href="browse-jobs.html">Find Standers</a></li>
+        <li><a href="post-job.html">Post A Job</a></li>
+        <li><a href="login.html">Login</a></li>
+        <li><a href="register.html">Sign Up</a></li>
+      </ul>
+    </nav>
+  </div>
+</header>
+`);
